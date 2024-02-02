3f9ca0e7f65255014a178b2dd416350195db95ab
Litosh Ruslan <litoshruslan@gmail.com>
2024-02-02 04:56:39 +0300

    fixed install issues

--- _build/config.inc.php
+++ _build/config.inc.php
@@ -9,7 +9,7 @@ if (!defined('MODX_CORE_PATH')) {
 }
 
 $name = 'LoginUp';
-$version = '1.0.9';
+$version = '1.1.0';
 $release = 'pl';
 
 return [
--- _build/resolvers/encryption.php
+++
@@ -1,3 +0,0 @@
-<?php
-$transport->xpdo->loadClass('transport.xPDOObjectVehicle', XPDO_CORE_PATH, true, true);
-$transport->xpdo->loadClass('EncryptedVehicle', MODX_CORE_PATH . 'components/' . strtolower($transport->name) . '/model/', true, true);
